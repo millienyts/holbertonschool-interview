073b643d2c67b5586a85d2f2ad27ff17f4dd9a35
Millienyts <tmillieny@gmail.com>
2024-11-17 21:17:03 -0400

    interview

---
+++ menger/menger.h
@@ -0,0 +1,17 @@
+#ifndef MENGER_SPONGE_H
+#define MENGER_SPONGE_H
+
+#include <math.h>
+#include <stdio.h>
+
+void menger(int level);
+
+#endif#ifndef MENGER_SPONGE_H
+#define MENGER_SPONGE_H
+
+#include <math.h>
+#include <stdio.h>
+
+void menger(int level);
+
+#endif
