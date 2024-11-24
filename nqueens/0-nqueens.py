5298b151a29fb266db7494fe7dbda44d0f1c0ec6
Millienyts <tmillieny@gmail.com>
2024-11-24 16:01:46 -0400

    nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-Script that solves the N queens problem based on the general Backtracking
-algorithm.
-
-This is:
-procedure bt(c) is
-    if reject(P, c) then return
-    if accept(P, c) then output(P, c)
-    s  first(P, c)
-    while s  NULL do
-        bt(s)
-        s  next(P, s)
+Script that solves the N queens problem using the Backtracking algorithm.
 """
+
 import sys
 
 
 def valid_pos(solution, pos):
     """
-    Function that verifies if the position is valid
+    Checks if a position is valid for placing a queen.
+    A position is invalid if it conflicts with any already placed queen.
     """
     for queen in solution:
+        # Check if another queen is in the same column
         if queen[1] == pos[1]:
             return False
+        # Check diagonal conflicts
         if (queen[0] + queen[1]) == (pos[0] + pos[1]):
             return False
         if (queen[0] - queen[1]) == (pos[0] - pos[1]):
@@ -31,9 +25,9 @@ def valid_pos(solution, pos):
 
 def solve_queens(row, n, solution):
     """
-    Function that finds the solution recursively, from the root down
+    Recursively tries to place queens on the board and prints valid solutions.
     """
-    if (row == n):
+    if row == n:
         print(solution)
     else:
         for col in range(n):
@@ -41,32 +35,31 @@ def solve_queens(row, n, solution):
             if valid_pos(solution, pos):
                 solution.append(pos)
                 solve_queens(row + 1, n, solution)
-                solution.remove(pos)
+                solution.pop()  # Backtrack by removing the last queen
 
 
 def main(n):
     """
-    Main function
+    Main function to solve the N queens problem.
     """
     solution = []
-    """ From root(0) down(n) """
     solve_queens(0, n, solution)
 
 
 if __name__ == '__main__':
-    """ Validate the arguments from OS """
+    # Validate the number of arguments
     if len(sys.argv) != 2:
         print('Usage: nqueens N')
         sys.exit(1)
+
     try:
-        i = int(sys.argv[1])
-    except BaseException:
+        n = int(sys.argv[1])
+    except ValueError:
         print('N must be a number')
         sys.exit(1)
-    i = int(sys.argv[1])
-    if i < 4:
+
+    if n < 4:
         print('N must be at least 4')
         sys.exit(1)
 
-    """ Calling the main function """
-    main(i)
+    main(n)
