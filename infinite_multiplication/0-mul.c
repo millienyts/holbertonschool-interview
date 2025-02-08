3cf5c450f42ba8cf8ee5e017d58c9e17dbfcb490
Millienyts <tmillieny@gmail.com>
2025-02-08 02:04:09 -0400

    interview

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -24,22 +24,19 @@ int main(int argc, char *argv[])
     return (0);
 }
 
-/*corergir lo anterior, porq solo compara el primer caracter*/
-
 /**
  * multiply - Fills an array with the multiplication num_1 times i
  * Arguments
- * @num_1:	string representing the 1st number in 10 base
- * @num_2:  string representing the 2nd number in 10 base
- * Returns - The multiplication
+ * @num_1: string representing the 1st number in 10 base
+ * @num_2: string representing the 2nd number in 10 base
+ * Returns: The multiplication
  */
-
 void multiply(char *num_1, char *num_2)
 {
     char mul[10][MAX1], answer[MAX1][MAX2];
     int tag, i, j, len_1, len_2, res, value, pos_1, pos_2, row, z;
 
-    for (len_1 = str_len(num_1), len_2 = str_len(num_2), i = 0; i < 10; i++)
+    for (len_1 = str_len(num_1), len_2 = str_len(num_2), i = 0; i < 10; i++)      
         for (j = 0; j < MAX1; j++)
             mul[i][j] = '.';
 
@@ -87,6 +84,8 @@ void multiply(char *num_1, char *num_2)
  */
 int is_a_number(char *a_str)
 {
+    if (*a_str == '\0') 
+        return (_FALSE_);
     while (*a_str != '\0')
     {
         if (*a_str < '0' || *a_str > '9')
@@ -97,62 +96,49 @@ int is_a_number(char *a_str)
 }
 
 /**
- * sum_and_print - Generates the sumation and print the results
+ * sum_and_print - Generates the summation and prints the result
  * Arguments
- * @a:	matrix with multiplitation tables
- * @len_1: len of string 1
- * @len_2: len os string 2
- * Return: noting.
+ * @a: matrix with multiplication tables
+ * @len_1: length of string 1
+ * @len_2: length of string 2
+ * Return: nothing.
  */
 void sum_and_print(char a[MAX1][MAX2], int len_1, int len_2)
 {
-    int tag, i, j, sum, value;
-    char total[MAX2], ans[MAX2 - 1];
-    char *ar;
+    int tag = 0, sum, value;
+    char total[MAX2], ans[MAX2 - 1], *ar;
+    int j = 0;
 
-    /* 3. Add each column from left to right */
-    tag = 0;
-    for (j = 0; j <= (len_1 + len_2); j++)
-    {
+    // 3. Add each column from left to right
+    for (int i = 0; i <= (len_1 + len_2); i++) {
         sum = 0;
-        for (i = 0; i < len_2; i++)
-            sum += (a[i][j] >= '0' && a[i][j] <= '9') ? a[i][j] - 48 : 0;
-        value = (sum + tag) % 10; /* las digit added */
-        total[j] = (value + '0'); /* converted to*/
+        for (int k = 0; k < len_2; k++) {
+            if (a[k][i] >= '0' && a[k][i] <= '9') 
+                sum += a[k][i] - '0';
+        }
+        value = (sum + tag) % 10;
+        total[i] = (value + '0');
         tag = (sum + tag) / 10;
     }
 
-    /* Reverse the string */
-    for (i = 0; i < len_1 + len_2; i++)
-    {
-        ans[i] = total[len_1 + len_2 - 1 - i];
-    }
-    ans[i] = '\0';
+    // Find first non-zero character
+    while (j < len_1 + len_2 && total[j] == '0') 
+        j++;
 
-    /* remove 0-s from the left side of the string */
-    value = str_len(ans);
-    for (i = 0; i < value; i++)
-    {
-        if (ans[i] == '0')
-            ar = &ans[i + 1];
-        else
-        {
-            ar = &ans[i];
-            i = value;
-        }
-    }
-    for (i = 0; ar[i] != '\0'; i++)
-    {
-        _putchar(ar[i]);
-    }
+    if (j == len_1 + len_2) 
+        _putchar('0'); // handle "0" case
+
+    for (; j < len_1 + len_2; j++) 
+        _putchar(total[j]);
+
     putchar('\n');
 }
 
 /**
- * str_len - Calculates the lenght of a string
+ * str_len - Calculates the length of a string
  * Arguments
- * @a:	matrix with multiplitation tables
- * Return: The lengnth of a.
+ * @a: string to calculate the length of
+ * Return: The length of the string.
  */
 int str_len(char *a)
 {
