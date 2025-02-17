ec781bda09857e5a205e0e71816e8029ca0591b4
Millienyts <tmillieny@gmail.com>
2025-02-17 01:06:37 -0400

    nqueens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,66 +1,45 @@
 #!/usr/bin/python3
-
 """
-Script that solves the N queens problem using the Backtracking algorithm.
+N Queens: Solves the N Queens problem using backtracking.
 """
-
 import sys
 
-
-def valid_pos(solution, pos):
-    """
-    Checks if a position is valid for placing a queen.
-    A position is invalid if it conflicts with any already placed queen.
-    """
-    for queen in solution:
-        # Check if another queen is in the same column
-        if queen[1] == pos[1]:
-            return False
-        # Check diagonal conflicts
-        if (queen[0] + queen[1]) == (pos[0] + pos[1]):
-            return False
-        if (queen[0] - queen[1]) == (pos[0] - pos[1]):
+def is_safe(board, row, col, N):
+    """Check if a queen can be placed on board[row][col] without conflicts."""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
     return True
 
+def solve_nqueens(N, row=0, board=[]):
+    """Recursively solves the N Queens problem."""
+    if row == N:
+        print([[i, board[i]] for i in range(N)])
+        return
+    
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            solve_nqueens(N, row + 1, board + [col])
 
-def solve_queens(row, n, solution):
-    """
-    Recursively tries to place queens on the board and prints valid solutions.
-    """
-    if row == n:
-        print(solution)
-    else:
-        for col in range(n):
-            pos = [row, col]
-            if valid_pos(solution, pos):
-                solution.append(pos)
-                solve_queens(row + 1, n, solution)
-                solution.pop()  # Backtrack by removing the last queen
-
-
-def main(n):
-    """
-    Main function to solve the N queens problem.
-    """
-    solution = []
-    solve_queens(0, n, solution)
-
-
-if __name__ == '__main__':
-    # Validate the number of arguments
+def main():
+    """Main entry point for the N Queens solver."""
     if len(sys.argv) != 2:
-        print('Usage: nqueens N')
+        print("Usage: nqueens N")
         sys.exit(1)
-
+    
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
-        print('N must be a number')
+        print("N must be a number")
         sys.exit(1)
-
-    if n < 4:
-        print('N must be at least 4')
+    
+    if N < 4:
+        print("N must be at least 4")
         sys.exit(1)
+    
+    solve_nqueens(N)
 
-    main(n)
+if __name__ == "__main__":
+    main()
