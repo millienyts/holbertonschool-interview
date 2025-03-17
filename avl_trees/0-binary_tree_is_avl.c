988e06edf5dff26989b8f18f829d817e4c9a3adc
Millienyts <tmillieny@gmail.com>
2025-03-16 20:49:02 -0400

    trees

--- avl_trees/0-binary_tree_is_avl.c
+++ avl_trees/0-binary_tree_is_avl.c
@@ -8,17 +8,17 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int check = 0;
+	int check = 0;
 
-    if (tree == NULL)
-        return (0);
-    check = is_bst_recursive(tree, INT_MIN, INT_MAX);
-    if (check == 0)
-        return (0);
-    check = is_balanced_preorder(tree);
-    if (check == 0)
-        return (0);
-    return (1);
+	if (tree == NULL)
+		return (0);
+	check = is_bst_recursive(tree, INT_MIN, INT_MAX);
+	if (check == 0)
+		return (0);
+	check = is_balanced_preorder(tree);
+	if (check == 0)
+		return (0);
+	return (1);
 }
 
 /**
@@ -27,27 +27,27 @@ int binary_tree_is_avl(const binary_tree_t *tree)
  * @min: minimum value root node must be greater than
  * @max: maximum value root node must be less than
  *
- * Return: 1 if succesful, 0 if failed
+ * Return: 1 if successful, 0 if failed
  */
 int is_bst_recursive(const binary_tree_t *tree, int min, int max)
 {
-    int local_min = min, local_max = max, left = 0, right = 0;
+	int local_min = min, local_max = max, left = 0, right = 0;
 
-    if (tree == NULL)
-        return (1);
-    if (tree->n == min || tree->n == max)
-        return (0);
-    if (tree->left)
-        local_min = tree->left->n;
-    if (tree->right)
-        local_max = tree->right->n;
-    if (tree->n <= local_min || tree->n >= local_max)
-        return (0);
-    if (local_min == min && local_max == max)
-        return (1);
-    left = is_bst_recursive(tree->left, min, tree->n);
-    right = is_bst_recursive(tree->right, tree->n, max);
-    return (left * right);
+	if (tree == NULL)
+		return (1);
+	if (tree->n == min || tree->n == max)
+		return (0);
+	if (tree->left)
+		local_min = tree->left->n;
+	if (tree->right)
+		local_max = tree->right->n;
+	if (tree->n <= local_min || tree->n >= local_max)
+		return (0);
+	if (local_min == min && local_max == max)
+		return (1);
+	left = is_bst_recursive(tree->left, min, tree->n);
+	right = is_bst_recursive(tree->right, tree->n, max);
+	return (left * right);
 }
 
 /**
@@ -60,21 +60,21 @@ int is_bst_recursive(const binary_tree_t *tree, int min, int max)
  */
 int is_balanced_preorder(const binary_tree_t *tree)
 {
-    int balanced = 0;
+	int balanced = 0;
 
-    if (tree == NULL)
-        return (1);
+	if (tree == NULL)
+		return (1);
 
-    balanced = is_balanced(tree);
-    if (balanced == 0)
-        return (0);
-    balanced = is_balanced_preorder(tree->left);
-    if (balanced == 0)
-        return (0);
-    balanced = is_balanced_preorder(tree->right);
-    if (balanced == 0)
-        return (0);
-    return (1);
+	balanced = is_balanced(tree);
+	if (balanced == 0)
+		return (0);
+	balanced = is_balanced_preorder(tree->left);
+	if (balanced == 0)
+		return (0);
+	balanced = is_balanced_preorder(tree->right);
+	if (balanced == 0)
+		return (0);
+	return (1);
 }
 
 /**
@@ -86,19 +86,19 @@ int is_balanced_preorder(const binary_tree_t *tree)
  */
 int is_balanced(const binary_tree_t *tree)
 {
-    int left = 0, right = 0, balance_factor = 0;
+	int left = 0, right = 0, balance_factor = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left = height_helper(tree->left);
-    right = height_helper(tree->right);
+	left = height_helper(tree->left);
+	right = height_helper(tree->right);
 
-    balance_factor = left - right;
-    if (balance_factor >= -1 && balance_factor <= 1)
-        return (1);
+	balance_factor = left - right;
+	if (balance_factor >= -1 && balance_factor <= 1)
+		return (1);
 
-    return (0);
+	return (0);
 }
 
 /**
@@ -109,16 +109,16 @@ int is_balanced(const binary_tree_t *tree)
  */
 size_t height_helper(const binary_tree_t *tree)
 {
-    size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left = (1 + height_helper(tree->left));
-    right = (1 + height_helper(tree->right));
+	left = (1 + height_helper(tree->left));
+	right = (1 + height_helper(tree->right));
 
-    if (left > right)
-        return (left);
+	if (left > right)
+		return (left);
 
-    return (right);
+	return (right);
 }
